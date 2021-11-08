e0a12ece2034a1fef06ade52d216091669f1fa02
Nozyp <friosupr127@gmail.com>
2021-11-08 16:24:23 -0500

    Puerto y debug mode

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
 App = Flask(__name__)
+PORT = 5000
+DEBUG = True
 
 @App.route('/')
 def Principal():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    App.run(debug=True)
+    App.run(port = PORT, debug=DEBUG)
 
